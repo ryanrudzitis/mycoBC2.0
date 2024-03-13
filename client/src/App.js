de53a3ac4a6ea2bf6c130d8867f19f2432803fe1
ryanrudzitis <ryanrudzitis@icloud.com>
2024-03-13 13:24:11 -0700

    removed unused code

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
 
 // We import all the components we need in our app
-import Header from "./components/Header";
 import ShowCards from "./components/showCards";
 import Edit from "./components/edit";
 import Create from "./components/create";
-
-const styles = {
-  backgroundImage: `url(/trees.jpg)`,
-  backgroundPosition: 'fixed',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundAttachment: 'fixed',
-  // height: "100%",
-  // filter: "blur(5px)"
-  // backdropFilter: "blur(5px)"
-};
 
 const App = () => {
   return (
